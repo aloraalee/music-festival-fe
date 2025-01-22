9933e5ea0a6ece3cde86ff230af0a4b57bcb2ffa
Alora Riley <alora.riley@gmail.com>
2025-01-22 11:58:15 -0500

    feat: Install router.

---
+++ src/ScheduleContainer.js
@@ -0,0 +1,12 @@
+import SchedulePoster from '../src/SchedulePoster';
+
+function SchedulesContainer({ }) {
+
+  return (
+    <section className="schedules-container">
+      Schedule Container Here
+    </section>
+  )
+  }
+
+export default SchedulesContainer;
