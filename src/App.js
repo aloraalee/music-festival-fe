71cb651475e6309a9d614d90409644a7451c6501
Alora Riley <alora.riley@gmail.com>
2025-01-22 17:47:07 -0500

    feat: Add functionality for searching a title of a schedule.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useState, useEffect } from 'react';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route, useLocation} from 'react-router-dom';
 import ScheduleContainer from '../src/ScheduleContainer.js'
 import ScheduleDetails from '../src/ScheduleDetails.js'
 import './App.css';
 
 function App() {
     const [schedules, setSchedules] = useState([])
+    const [searchQuery, setSearchQuery] = useState('')
+    const location = useLocation()
   
     useEffect( () => {
       getSchedules()
@@ -22,13 +24,25 @@ function App() {
       .catch(error => console.log(error))
     }
 
+    const filteredSchedules = schedules.filter(schedule =>
+      schedule.attributes.title.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+
   return (
     <main className='App'>
       <header>
         <h1>Music Festival Schedules</h1>
+        {location.pathname==='/' && (
+          <input className='search-bar'
+            type="text"
+            placeholder="Search for a schedule"
+            value={searchQuery}
+            onChange={(event) => setSearchQuery(event.target.value)}
+          />
+        )}
       </header>
       <Routes>
-        <Route path='/' element={<ScheduleContainer schedules={schedules}/>}/>
+        <Route path='/' element={<ScheduleContainer schedules={filteredSchedules}/>}/>
         <Route path='/:id' element={<ScheduleDetails/>} />
       </Routes>
     </main>
