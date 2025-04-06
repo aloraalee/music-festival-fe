da5927157ad9b29f17f34d6ca40c4a682bed9f81
Alora Riley <alora.riley@gmail.com>
2025-04-05 20:08:13 -0500

    feat: create methods to display dates and showtimes in a more readable way.

--- src/SchedulePoster.js
+++ src/SchedulePoster.js
@@ -3,9 +3,15 @@ import { Link } from 'react-router-dom';
 
 function SchedulePoster({ title, date, id }) {
 
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
   return (
     <section className="schedule-poster">
-      <h2>{title} for {date}</h2>
+      <h2>{title} for {formattedDate}</h2>
       <Link to={`/${id}`}>
       <img className="schedule-img"src="/porto.jpg" alt={`Poster of ${title}`}/>
       </Link>
--- src/ShowSection.js
+++ src/ShowSection.js
@@ -3,11 +3,16 @@ import './ShowSection.css';
 
 function ShowSection({ showId, artist, location, showTime, deleteShow }) {
 
+  const formattedTime = new Date(showTime).toLocaleString('en-US', {
+    dateStyle: 'long',
+    timeStyle: 'short',
+  });
+
   return (
     <section className="show-card">
       <p><strong>Artist: {artist}</strong></p>
       <p>Stage: {location}</p>
-      <p>Showtime: {showTime}</p>
+      <p>Showtime: {formattedTime}</p>
       <button className="delete-btn" onClick={() => deleteShow(showId)}>Delete show</button>
     </section>
     )}
