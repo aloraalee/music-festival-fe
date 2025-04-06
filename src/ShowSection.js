b8e1b5592f9053e6a3d8864322b3355b4c0501b3
Alora Riley <alora.riley@gmail.com>
2025-04-05 19:57:26 -0500

    feat: Update ShowSection styling to have cards with space and styling.

--- src/ShowSection.js
+++ src/ShowSection.js
@@ -1,7 +1,10 @@
+import './ShowSection.css';
+
+
 function ShowSection({ showId, artist, location, showTime, deleteShow }) {
 
   return (
-    <section className="show-section">
+    <section className="show-card">
       <p><strong>Artist: {artist}</strong></p>
       <p>Stage: {location}</p>
       <p>Showtime: {showTime}</p>
